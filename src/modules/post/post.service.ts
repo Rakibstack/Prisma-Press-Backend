8bf304b8e445145c6d4f63fd5344547bb4047338
Rakibstack <rh6039511@gmail.com>
2026-07-16 20:23:15 +0600

    optimizing the search,filter,sorting and pagination

--- src/modules/post/post.service.ts
+++ src/modules/post/post.service.ts
@@ -1,6 +1,11 @@
 import { CommentStatus, PostStatus } from "../../../generated/prisma/enums";
+import { PostWhereInput } from "../../../generated/prisma/models";
 import { prisma } from "../../lib/prisma";
-import { IcreatePostPayload, IpostQuery, IupdatePostPayload } from "./post.interface";
+import {
+  IcreatePostPayload,
+  IpostQuery,
+  IupdatePostPayload,
+} from "./post.interface";
 
 const createPostIntoDB = async (
   payload: IcreatePostPayload,
@@ -15,47 +20,65 @@ const createPostIntoDB = async (
   return createPost;
 };
 
-const getAllPostFromDB = async (query :IpostQuery) => {
-
+const getAllPostFromDB = async (query: IpostQuery) => {
   const limit = query.limit ? Number(query.limit) : 10;
-  const page = query.page ? (Number(query.page)) : 1;
+  const page = query.page ? Number(query.page) : 1;
   const skip = (page - 1) * limit;
-  const sortBy= query.sortBy ? query.sortBy : 'createdAt';
-  const sortOrder = query.sortOrder ? query.sortOrder : "desc"
-  const result = await prisma.post.findMany({
+  const sortBy = query.sortBy ? query.sortBy : "createdAt";
+  const sortOrder = query.sortOrder ? query.sortOrder : "desc";
 
-   
-      where: {
-       AND: [
-           
-         query.searchTerm ? {
-          OR : [
-            {
-               title : {
-                contains:query.searchTerm,
-                mode: "insensitive"
-               }
-            },
-            {
-               content : {
-                contains:query.searchTerm,
-                mode: "insensitive"
-               }
-            }
-          ]
-         } : {},
+  const tags = query.tags ? JSON.parse(query.tags as string) : null;
+  const tagsArray = Array.isArray(tags) ? tags : [];
 
-         query.title ? {title : query.title } : {},
-         query.content ? {content : query.content} : {}, 
-       ]
-       
+  const addConditions: PostWhereInput[] = [];
+
+  if (query.searchTerm) {
+    addConditions.push({
+      OR: [
+        {
+          title: {
+            contains: query.searchTerm,
+            mode: "insensitive",
+          },
+        },
+        {
+          content: {
+            contains: query.searchTerm,
+            mode: "insensitive",
+          },
+        },
+      ],
+    });
+  }
+  if (query.title) {
+    addConditions.push({
+      title: query.title,
+    });
+  }
+  if (query.content) {
+    addConditions.push({
+      content: query.content,
+    });
+  }
+
+  if (query.tags) {
+    addConditions.push({
+      tags: {
+        hasSome: tagsArray,
       },
-      take:limit,
-      skip: skip,
+    });
+  }
 
-      orderBy: {
-        [sortBy] : sortOrder
-      }
+  const result = await prisma.post.findMany({
+    where: {
+      AND: addConditions,
+    },
+    take: limit,
+    skip: skip,
+
+    orderBy: {
+      [sortBy]: sortOrder,
+    },
   });
 
   return result;
