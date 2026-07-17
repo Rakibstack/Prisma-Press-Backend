6d6985c2d15c6ab00e5881d38af2c982d3dc6555
Rakibstack <rh6039511@gmail.com>
2026-07-17 17:04:53 +0600

    Handle Prisma Error With Golobal Error Hndler

--- src/middlewares/golobalErrorHandler.ts
+++ src/middlewares/golobalErrorHandler.ts
@@ -9,23 +9,39 @@ export const globalErrorHandler = (
   res: Response,
   next: NextFunction,
 ) => {
+  let statusCode;
+  let errorMessage = err.message || "Internal Server Error";
+  let errorName = err.name || "Internal Server Error";
 
-    let statusCode;
-    let errorMessage = err.message || 'Internal Server Error';
-    let errorName = err.name || 'Internal Server Error';
-
-    if( err instanceof Prisma.PrismaClientValidationError){
-      statusCode = httpstatus.BAD_REQUEST,
-      errorMessage = 'you have provided incorrect field type or missing fields'
-     } else if (err instanceof Prisma.PrismaClientKnownRequestError){
-        if(err.code === 'P2002'){
-            statusCode = httpstatus.BAD_REQUEST,
-            errorMessage = 'Duplicate Key Error'
-        }else if (err.code === 'P2003'){
-            statusCode = httpstatus.BAD_REQUEST,
-            errorMessage = "Foreign Key Constraint Failed"
-        }
-     }
+  if (err instanceof Prisma.PrismaClientValidationError) {
+    ((statusCode = httpstatus.BAD_REQUEST),
+      (errorMessage =
+        "you have provided incorrect field type or missing fields"));
+  } else if (err instanceof Prisma.PrismaClientKnownRequestError) {
+    if (err.code === "P2002") {
+      ((statusCode = httpstatus.BAD_REQUEST),
+        (errorMessage = "Duplicate Key Error"));
+    } else if (err.code === "P2003") {
+      ((statusCode = httpstatus.BAD_REQUEST),
+        (errorMessage = "Foreign Key Constraint Failed"));
+    } else if (err.code === "P2025") {
+      ((statusCode = httpstatus.BAD_REQUEST),
+        (errorMessage =
+          "An operation failed because it depends on one or more records that were required but not found"));
+    }
+  } else if (err instanceof Prisma.PrismaClientInitializationError) {
+    if (err.errorCode === "P1000") {
+      ((statusCode = httpstatus.BAD_REQUEST),
+        (errorMessage =
+          " Authentication failed against database server.Please Check You Credentials"));
+    } else if (err.errorCode === "P1001") {
+      statusCode = httpstatus.BAD_REQUEST;
+      errorMessage = "Can't reach database server";
+    }
+  } else if (err instanceof Prisma.PrismaClientUnknownRequestError){
+    statusCode =httpstatus.INTERNAL_SERVER_ERROR
+    errorMessage = 'Error Occurred During Query Executing'
+  }
 
   res.status(httpstatus.INTERNAL_SERVER_ERROR).json({
     success: false,
